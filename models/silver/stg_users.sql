6e50b8fc038e7d6fb25994cf04c8e3a9caa79d3e
Luis Grados Salinas <martin.programmer@gmail.com>
2022-10-19 13:25:54 -0500

    Update stg_users.sql

--- models/silver/stg_users.sql
+++ models/silver/stg_users.sql
@@ -1,27 +1,13 @@
+{{ config(schema = 'stackoverflow_silver') }}
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
+with source as (
+    select
+  id as user_id,
+  age,
+  creation_date,
+  round(timestamp_diff(current_timestamp(), creation_date, day)/365) as user_tenure
+from
+ {{ source('bronze', 'users') }}
 )
 
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select * from source
